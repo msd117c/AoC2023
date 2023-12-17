276c1d29a4eed0b3628c97bfed1f7028e2e205da
Miguel Sánchez <msd117c@users.noreply.github.com>
2023-12-17 19:08:49 +0100

    Minor refactor

--- src/main/java/days/Day17.kt
+++ src/main/java/days/Day17.kt
@@ -3,7 +3,6 @@ package days
 import utils.InputReader.readInput
 import kotlin.math.abs
 
-
 object Day17 {
 
     private enum class Direction(val coordinates: Coordinates) {
@@ -41,7 +40,6 @@ object Day17 {
     }
 
     private fun Array<CharArray>.dijkstra(start: Coordinates): Map<Coordinates, Pair<MutableList<Movement>, Int>> {
-        val distances = mutableMapOf(start to 0)
         val paths = mutableMapOf(start to (mutableListOf<Movement>() to 0))
         val queue = mutableListOf(start to mutableListOf(Movement(start, Direction.HORIZONTAL)))
 
@@ -50,16 +48,16 @@ object Day17 {
 
             val neighbors = findNeighbors(current, path)
 
-            for (neighbor in neighbors) {
-                val newDistance = distances[current]!! + neighbor.weight
+            neighbors.forEach { neighbor ->
+                val newDistance = paths[current]!!.second + neighbor.weight
 
-                if (!distances.containsKey(neighbor.coordinates) || newDistance < distances[neighbor.coordinates]!!) {
-                    distances[neighbor.coordinates] = newDistance
-
+                if (!paths.containsKey(neighbor.coordinates) || newDistance < paths[neighbor.coordinates]!!.second) {
                     val newMovement = getMovement(current, neighbor.coordinates)
-                    val newPath = neighbor.coordinates to (path + listOf(newMovement)).toMutableList()
-                    queue.add(newPath)
-                    paths[neighbor.coordinates] = (path + listOf(newMovement)).toMutableList() to newDistance
+                    val newPath = (path + listOf(newMovement)).toMutableList()
+                    paths[neighbor.coordinates] = newPath to newDistance
+
+                    val newValue = neighbor.coordinates to newPath
+                    queue.add(newValue)
                 }
             }
         }
@@ -84,31 +82,35 @@ object Day17 {
     private fun Array<CharArray>.findNeighbors(coordinates: Coordinates, path: List<Movement>): List<Path> {
         val lastMovements = path.takeLast(3)
 
-        val latest = path.lastOrNull()?.coordinates
+        val latest = path.dropLast(1).lastOrNull()?.coordinates
 
-        val nextOptions = if (lastMovements.size == 3 && lastMovements.map { it.direction }.distinct().size == 1) {
-            when (lastMovements.map { it.direction }.last()) {
-                Direction.VERTICAL -> {
+        val allOptions = listOf(
+            Coordinates(x = coordinates.x + 1, y = coordinates.y),
+            Coordinates(x = coordinates.x - 1, y = coordinates.y),
+            Coordinates(x = coordinates.x, y = coordinates.y + 1),
+            Coordinates(x = coordinates.x, y = coordinates.y - 1),
+        )
+
+        val nextOptions = if (lastMovements.size == 3) {
+            when {
+                lastMovements.all { it.direction == Direction.VERTICAL } -> {
                     listOf(
                         Coordinates(x = coordinates.x + 1, y = coordinates.y),
                         Coordinates(x = coordinates.x - 1, y = coordinates.y),
                     )
                 }
 
-                Direction.HORIZONTAL -> {
+                lastMovements.all { it.direction == Direction.HORIZONTAL } -> {
                     listOf(
                         Coordinates(x = coordinates.x, y = coordinates.y + 1),
                         Coordinates(x = coordinates.x, y = coordinates.y - 1),
                     )
                 }
+
+                else -> allOptions
             }
         } else {
-            listOf(
-                Coordinates(x = coordinates.x + 1, y = coordinates.y),
-                Coordinates(x = coordinates.x - 1, y = coordinates.y),
-                Coordinates(x = coordinates.x, y = coordinates.y + 1),
-                Coordinates(x = coordinates.x, y = coordinates.y - 1),
-            )
+            allOptions
         }
 
         return nextOptions
